bc40630375bb29075a2423d5cddfefa58fca0084
DaSwow <carls09995@hotmail.com>
2021-11-05 22:58:16 -0700

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7f3ff29a452c6a9f00c3db7fd32a27070f4ef9bc',
+        'name' => 'vendor/metagip',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/metagip' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7f3ff29a452c6a9f00c3db7fd32a27070f4ef9bc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
