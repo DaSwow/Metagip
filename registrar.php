eb797667cb0cbcaf40d9b12a250178769c8f4b5e
DaSwow <carls09995@hotmail.com>
2021-11-06 06:45:01 -0700

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -9,7 +9,7 @@
     $name=$_POST['nombre'];
 
     $query="SELECT * FROM public.usuarios WHERE correo='$mail' ";
-    $consulta= pg_query_params($db,$query);
+    $consulta= pg_query_params($conexion,$query);
     $cantidad= pg_num_rows($consulta);
     if(cantidad>0){
         $query="INSERT INTO public.usuarios(correo, contrasenia, nombre)VALUES ($mail, $password, $name);";
