d338970d807f1c28e9e0659858d23b0ecec320c5
DaSwow <carls09995@hotmail.com>
2021-11-06 09:04:49 -0700

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -2,14 +2,13 @@
     require 'db.php';
     session_start();
     $mail=$_POST['correo'];
-    echo $mail;
     $password=$_POST['contrasenia'];
     $name=$_POST['nombre'];
 
     $query="SELECT * FROM public.usuarios WHERE correo='$mail'";
     $consulta= pg_query($conexion,$query);
     $cantidad= pg_num_rows($consulta);
-    if(cantidad>0){
+    if(cantidad==0){
         $query="INSERT INTO public.usuarios(correo, contrasenia, nombre) VALUES ($mail, $password, $name);";
          header("location: index.php");
         echo "Cuenta registrada exitosamente";
