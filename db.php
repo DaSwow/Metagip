f4734f11a358b7b51e0aea17df12230930b851b6
DaSwow <carls09995@hotmail.com>
2021-11-06 06:52:10 -0700

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
         $db = parse_url(getenv("DATABASE_URL"));
         $db["path"] = ltrim($db["path"], "/");
-        $string_conexion = "host=$db[1] dbname=$db[5] port=$db[2] user=$db[3] password=$db[4]";
+        $string_conexion = "host=ec2-3-226-211-228.compute-1.amazonaws.com dbname=dcjv6v0os1b0hh port=5432 user=dazlcuyqhdonwj password=6417c3ad9bbfea318806931ed986394fff42683703d75e8515f3329966d4b41f]";
         $conexion = pg_connect($connection_string) or die('failed');
         
 ?>
