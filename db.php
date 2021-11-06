fa2efe45e3c8adbb6a182611e684dbe241829d71
DaSwow <carls09995@hotmail.com>
2021-11-06 07:57:51 -0700

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@
         $port = $db['port'];
         $user = $db['user'];
         $password = $db['user'];
-        $string_conexion = "$host $dbname $port $user $password";
+        $string_conexion = "host=$host dbname=$dbname port=$port user=$user pass=$password";
         echo $host;
         //$conexion = pg_connect($db) or die('failed');
         
