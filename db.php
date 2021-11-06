773c4cd68dd642a5f7b45171563d535c94de54b0
DaSwow <carls09995@hotmail.com>
2021-11-06 06:28:08 -0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
         $db = parse_url(getenv("DATABASE_URL"));
         $db["path"] = ltrim($db["path"], "/");
-        $conexion = pg_connect("host=$db[1] dbname=$db[5] user=$db[3] password=$db[4]");
+        $conexion = pg_connect("host=$db[1] dbname=$db[5] user=$db[3] password=$db[4]") or die('failed');
 ?>
