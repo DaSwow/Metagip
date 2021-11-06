76e4aca9b9f6e1d71d677e3e7c468cc92148c0e2
DaSwow <carls09995@hotmail.com>
2021-11-06 00:11:46 -0700

    c

--- registrar.php
+++ registrar.php
@@ -1,15 +1,15 @@
 <?php
     require 'db.php';
     session_start();
-    $correo=$_POST['mail'];
-    $contrasenia=$_POST['password'];
-    $nombre=$_POST['name'];
+    $mail=$_POST['correo'];
+    $password=$_POST['contrasenia'];
+    $name=$_POST['nombre'];
 
-    $query="SELECT * FROM public.usuarios WHERE correo='$correo' ";
+    $query="SELECT * FROM public.usuarios WHERE correo='$mail' ";
     $consulta= pg_query($conexion,$query);
     $cantidad= pg_num_rows($consulta);
     if(cantidad>0){
-        $query="INSERT INTO public.usuarios(correo, contrasenia, nombre)VALUES ($correo, $contrasenia, $nombre);";
+        $query="INSERT INTO public.usuarios(correo, contrasenia, nombre)VALUES ($mail, $password, $name);";
         header("location: index.php");
     }else{
             echo "Este correo ya esta registrado.";
