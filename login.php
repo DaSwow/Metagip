3941b7182894b856022dfdd58a292617913ffdd2
MichaelEAL <amrasdrako@gmail.com>
2021-11-07 03:24:56 -0700

    Update login.php

--- login.php
+++ login.php
@@ -9,11 +9,12 @@ $password = $_POST['logContrasenia'];
 $query = "SELECT * FROM public.usuarios WHERE correo='$mail' AND contrasenia='$password';";
 $consulta = pg_query($conexion, $query);
 $cantidad = pg_num_rows($consulta);
-$nombre = pg_query("SELECT nombre FROM public.usuarios WHERE correo='$mail';");
+//$nombre = pg_query("SELECT nombre FROM public.usuarios WHERE correo='$mail';");
+$nombre = pg_fetch_row($consulta);
 echo $cantidad;
 
 if ($cantidad === 1) {
-    echo "<script type='text/javascript'>alert('$nombre');location='index.php';</script>";
+    echo "<script type='text/javascript'>alert('$nombre[2]]');location='index.php';</script>";
 } else {
     echo "<script type='text/javascript'>alert('Datos incorrectos');location='index.php';</script>";
 }
