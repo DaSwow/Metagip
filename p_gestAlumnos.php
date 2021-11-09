cb12f1c63ddc16eba8b0592ba54570f7861c3fac
MichaelEAL <amrasdrako@gmail.com>
2021-11-09 00:42:47 -0700

    agregado gestionar alumnos

---
+++ p_gestAlumnos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="style.css">
+<title>Gestionar Alumnos</title>
+</head>
+
+
+
+<body>
+
+
+    <div class="container">
+        <div class="table-container">
+            <div class="menu" id="div1">
+                <h2>Gestionar Alumno</h2>
+                <nav>
+                    <form action="">
+                        <input type="submit" class="submit" value="Editar">
+                        <input type="submit" class="submit" value="Elimar">
+                        <input type="submit" class="submit" value="Agregar">
+                        <input type="submit" class="submit" value="Regresar">
+                    </form> 
+                </nav>
+                <table class="colm1"border="1">
+                    <tr>
+                        <td class="fila1"><p>ID</p></td>
+                        <td class="fila"><p>Nombre</p></td>
+                        <td class="fila"><p>Email</p></td>
+                    </tr>
+                    <tr>
+                        <td class="fila2"><p>00000189362</p></td>
+                        <td class="fila2"><p>Edith </p></td>
+                        <td class="fila2"><p>asdasdas@gmail.com</p></td>
+                    </tr>
+                </table>
+                <br>
+
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
