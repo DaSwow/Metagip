3bfd4ac27a3c20b8ff0c5bb98b1ab4d79009ba69
MichaelEAL <amrasdrako@gmail.com>
2021-11-08 20:46:21 -0700

    Menu agregado

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+  <link rel="stylesheet" href="style.css">
+ 
+  <title>Menu Principal</title>
+</head>
+<body>
+   <div class="container">
+     <div class="menu-container">
+       <div class="menu">
+         <h2>Menu Principal</h2>
+         <form action="">
+           <input type="submit" class="submit" value="Gestionar Alumnos">
+           <input type="submit" class="submit" value="Gestionar Cursos">
+           <input type="submit" class="submit" value="Registrar Alumnos">
+           <input type="submit" class="submit" value="Importar CVS">
+           <input type="submit" class="submit" value="Administrar Asistencias">
+           <input type="submit" class="submit" value="Log Out">
+         </form>
+       </div>
+     </div>
+   </div>
+</body>
+</html>
