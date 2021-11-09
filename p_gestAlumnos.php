59c9fa27f5295efaad2891b4e77db2afbd372ba1
Marxdd <63327100+Marxdd@users.noreply.github.com>
2021-11-09 14:04:33 -0700

    cambio de nombre y tabla dinamica

--- p_gestAlumnos.php
+++ p_gestAlumnos.php
@@ -1,3 +1,12 @@
+<?php
+include 'db.php';
+session_start();
+$correo_prof = $_SESSION['correo_usuario'];
+
+$query ="SELECT * FROM public.alumnos WHERE profesor='$correo_prof'";
+$consulta = pg_query($conexion, $query);
+
+?>
 <!DOCTYPE html>
 <link rel="stylesheet" href="style.css">
 <title>Gestionar Alumnos</title>
@@ -14,14 +23,19 @@
                             <td class="fila"><p>Nombre</p></td>
                             <td class="fila"><p>Email</p></td>
                         </tr>
+                        <?php while ($row = $consulta->fetch_assoc()){?>
                         <tr>
-                            <td class="fila2"><p>00000189362</p></td>
+                            <td><?php echo $row['id'];?></td>
+                            <td><?php echo $row['nombre'];?></td>
+                            <td><?php echo $row['correo'];?></td>
+                            <!--<td class="fila2"><p>00000189362</p></td>
                             <td class="fila2"><p>Edith </p></td>
-                            <td class="fila2"><p>asdasdas@gmail.com</p></td>
+                            <td class="fila2"><p>asdasdas@gmail.com</p></td>-->
                         </tr>
+                        <?php }?>
                     </table>
                     <nav>
-                        <form action="s_consultarAlumnos.php">
+                        <form action="">
                             <input type="submit" class="submit" value="Editar">
                             <input type="submit" class="submit" value="Elimar">
                             <input type="submit" class="submit" value="Agregar">
