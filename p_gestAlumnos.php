9753793929cb0242d6622cc41eb21ae2165d6764
MichaelEAL <amrasdrako@gmail.com>
2021-11-09 01:01:05 -0700

    UI changes

--- p_gestAlumnos.php
+++ p_gestAlumnos.php
@@ -2,37 +2,34 @@
 <link rel="stylesheet" href="style.css">
 <title>Gestionar Alumnos</title>
 </head>
-
-
-
 <body>
-
-
     <div class="container">
         <div class="table-container">
-            <div class="menu" id="div1">
+            <div class="menu">
                 <h2>Gestionar Alumno</h2>
-                <nav>
-                    <form action="">
-                        <input type="submit" class="submit" value="Editar">
-                        <input type="submit" class="submit" value="Elimar">
-                        <input type="submit" class="submit" value="Agregar">
-                        <input type="submit" class="submit" value="Regresar">
-                    </form> 
-                </nav>
-                <table class="colm1"border="1">
-                    <tr>
-                        <td class="fila1"><p>ID</p></td>
-                        <td class="fila"><p>Nombre</p></td>
-                        <td class="fila"><p>Email</p></td>
-                    </tr>
-                    <tr>
-                        <td class="fila2"><p>00000189362</p></td>
-                        <td class="fila2"><p>Edith </p></td>
-                        <td class="fila2"><p>asdasdas@gmail.com</p></td>
-                    </tr>
-                </table>
-                <br>
+                <div id="scroll">
+                    <table class="colm1"border="1">
+                        <tr>
+                            <td class="fila1"><p>ID</p></td>
+                            <td class="fila"><p>Nombre</p></td>
+                            <td class="fila"><p>Email</p></td>
+                        </tr>
+                        <tr>
+                            <td class="fila2"><p>00000189362</p></td>
+                            <td class="fila2"><p>Edith </p></td>
+                            <td class="fila2"><p>asdasdas@gmail.com</p></td>
+                        </tr>
+                    </table>
+                    <nav>
+                        <form action="">
+                            <input type="submit" class="submit" value="Editar">
+                            <input type="submit" class="submit" value="Elimar">
+                            <input type="submit" class="submit" value="Agregar">
+                            <input type="submit" class="submit" value="Regresar">
+                        </form> 
+                    </nav> 
+
+                </div>
 
             </div>
 
---
+++ p_gestCursos.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="style.css">
+<title>Gestionar Alumnos</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="table-container2">
+            <div class="menu" >
+                <div id="scroll">
+                    <h2>Gestionar Curso</h2>
+                    <nav>
+                        <form action="">
+                            <input type="submit" class="submit" value="Editar">
+                            <input type="submit" class="submit" value="Elimar">
+                            <input type="submit" class="submit" value="Agregar">
+                            <input type="submit" class="submit" value="Regresar">
+                        </form> 
+                    </nav>
+                    <table class="colm1"border="1">
+                        <tr>
+                            <td class="fila1"><p>Clave</p></td>
+                            <td class="fila"><p>Nombre</p></td>
+                            <td class="fila"><p>Hora-Inicio</p></td>
+                            <td class="fila"><p>Hora-Fin</p></td>
+                            <td class="fila"><p>Unidades</p></td>
+                        </tr>
+                        <tr>
+                            <td class="fila2"><p>00000189362</p></td>
+                            <td class="fila2"><p>Edith </p></td>
+                            <td class="fila2"><p>8:00</p></td>
+                            <td class="fila2"><p>9:30</p></td>
+                            <td class="fila2"><p>6</p></td>
+                        </tr>
+                    </table>
+                    <br>
+                </div>
+            </div>
+            <div class="unidad">
+                <h2>Unidades</h2>
+                <div class="unidad-items">
+                    <input type="text" placeholder="Inicio" class="inicio">
+                    <input type="text" placeholder="Fin" class="fin">
+                </div>
+            </div>
+        </div>
+</body>
+</html>
