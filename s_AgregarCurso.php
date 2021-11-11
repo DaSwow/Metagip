cd7eb377ba51332113b2f3f88e0ac98c65aa2d5a
DaSwow <carls09995@hotmail.com>
2021-11-11 07:09:19 -0700

    Update s_AgregarCurso.php

--- s_AgregarCurso.php
+++ s_AgregarCurso.php
@@ -11,6 +11,16 @@ $unidades = $_POST['unidades'];
 if ($horaFin < $horaIni) {
     echo "<script type='text/javascript'>alert('La hora de fin del curso no puede ser antes que la hora de inicio.');location='p_gestCursos.php';</script>";
 } else {
-    
+    $query ="SELECT * FROM public.cursos WHERE clave='$clave'";
+    $consulta = pg_query($conexion, $query);
+    $cantidad = pg_num_rows($consulta);
+    if ($cantidad === 0) {
+        $queryInsert = "INSERT INTO public.alumnos (id,nombre,correo,profesor) VALUES ('$id','$nombre_al','$correo_al','$correo_prof')";
+        pg_query($queryInsert);
+         echo "<script type='text/javascript'>alert('Alumno registrado exitosamente');location='p_gestAlumnos.php';</script>";
+    }else {
+        echo "<script type='text/javascript'>alert('Este alumno ya esta registrado.');location='p_gestAlumnos.php';</script>";
+    }
 }
+
 ?>
