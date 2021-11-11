297f241687a4b6575d54162a7e4ceb2ea58ab1d2
MichaelEAL <amrasdrako@gmail.com>
2021-11-11 05:25:14 -0700

    Update s_editAlumno.php

--- s_editAlumno.php
+++ s_editAlumno.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 require 'db.php';
@@ -13,8 +12,8 @@ if (!$consulta) {
     echo "<script type='text/javascript'>alert('Error de coneccion');location='p_gestAlumnos.php';</script>";
 }
 
-if (isset($_GET['update'])) {
-    $queryInsert = "UPDATE FROM public.alumnos (nombre,correo) VALUES ('$nombreal','$correoal') WHERE id = '$idAl'";
+function updateAlm(){
+    $queryInsert = "UPDATE FROM public.alumnos (nombre,correo) VALUES ('$alumno[1]]','$alumno[2]]') WHERE id = '$alumno[0]]'";
     pg_query($queryInsert);
     echo "<script type='text/javascript'>alert('Alumno registrado exitosamente');location='p_gestAlumnos.php';</script>";
 }
@@ -22,13 +21,23 @@ if (isset($_GET['update'])) {
 <!DOCTYPE html>
 <link rel="stylesheet" href="style.css">
 <title>Editar Alumno</title>
-
-<nav class="editAlumno">
-    <form action="s_editAlumnos.php?update=<?php echo $alumno[0]; ?>" method="post">
-        <input type="test" placeholder="ID" class="id" name="idAl" value="<?php echo $alumno[0]; ?>" readonly="readonly">
-        <input type="text" placeholder="nombre" class="nombre" name="nombreAl" value="<?php echo $alumno[1]; ?>" required>
-        <input type="text" placeholder="correo" class="correo" name="correoAl" value="<?php echo $alumno[2]; ?>" required>
-        <input type="submit" class="submit" value="actualizar" name="update">
-        update
-    </form>
-</nav>
+<body>
+    <div class="container">
+        <div class="table-container2">
+            <div class="menu" >
+                <div id="scroll">
+                    <div id="scroll">
+                        <nav class="editAlumno">
+                            <form method="post">
+                                <input type="test" placeholder="ID" class="id" name="idAl" value="<?php echo $alumno[0]; ?>" readonly="readonly">
+                                <input type="text" placeholder="nombre" class="nombre" name="nombreAl" value="<?php echo $alumno[1]; ?>" required>
+                                <input type="text" placeholder="correo" class="correo" name="correoAl" value="<?php echo $alumno[2]; ?>" required>
+                                <input type="submit" class="submit" value="actualizar" name="update" onclick="updateAlm()">
+                            </form>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
