0354b06a1f642c6ffbee3e9e4f16116f0dcf149a
DaSwow <carls09995@hotmail.com>
2021-11-11 07:04:33 -0700

    Update s_AgregarCurso.php

--- s_AgregarCurso.php
+++ s_AgregarCurso.php
@@ -1,13 +1,12 @@
 <?php
-
 require 'db.php';
 session_start();
 
 $clave = $_POST['clave'];
 $nombreCurso = $_POST['nombreCurso'];
 $horaIni = $_POST['horaIni'];
-$horaFin = $_POST['horaIni'];
-$unidades = $_POST['horaFin'];
+$horaFin = $_POST['horaFin'];
+$unidades = $_POST['unidades'];
 
 if ($horaFin < $horaIni) {
     echo "<script type='text/javascript'>alert('La hora de fin del curso no puede ser antes que la hora de inicio.');location='p_gestCursos.php';</script>";
