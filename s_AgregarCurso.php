423d28c54f58c8ef0a15e5730faed66c70742ffa
DaSwow <carls09995@hotmail.com>
2021-11-11 08:14:32 -0700

    Update s_AgregarCurso.php

--- s_AgregarCurso.php
+++ s_AgregarCurso.php
@@ -7,19 +7,22 @@ $nombreCurso = $_POST['nombreCurso'];
 $horaIni = $_POST['horaIni'];
 $horaFin = $_POST['horaFin'];
 $unidades = $_POST['unidades'];
+$correo_profesor = $_SESSION["correo_usuario"];
 
 if ($horaFin < $horaIni) {
     echo "<script type='text/javascript'>alert('La hora de fin del curso no puede ser antes que la hora de inicio.');location='p_gestCursos.php';</script>";
 } else {
-    $query ="SELECT * FROM public.cursos WHERE clave='$clave'";
+    $query ="SELECT * FROM public.curso WHERE clave='$clave'";
     $consulta = pg_query($conexion, $query);
     $cantidad = pg_num_rows($consulta);
     if ($cantidad === 0) {
-        $queryInsert = "INSERT INTO public.alumnos (id,nombre,correo,profesor) VALUES ('$id','$nombre_al','$correo_al','$correo_prof')";
+        $queryInsert = "INSERT INTO public.cursos(
+	clave, nombre, 'horaIni', 'horaFin', cursos, profesor)
+	VALUES ($clave, $nombreCurso,  $horaIni, $horaFin, $unidades, $correo_profesor );";
         pg_query($queryInsert);
-         echo "<script type='text/javascript'>alert('Alumno registrado exitosamente');location='p_gestAlumnos.php';</script>";
+         echo "<script type='text/javascript'>alert('Curso registrado exitosamente');location='p_gestCursos.php';</script>";
     }else {
-        echo "<script type='text/javascript'>alert('Este alumno ya esta registrado.');location='p_gestAlumnos.php';</script>";
+        echo "<script type='text/javascript'>alert('Este curso ya esta registrado.');location='p_gestCursos.php';</script>";
     }
 }
 
