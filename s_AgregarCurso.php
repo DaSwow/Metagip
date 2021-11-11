1cade7fa1d8bb7e1a4ef252f2dac7f15266ed89e
DaSwow <carls09995@hotmail.com>
2021-11-11 09:09:29 -0700

    Update s_AgregarCurso.php

--- s_AgregarCurso.php
+++ s_AgregarCurso.php
@@ -9,8 +9,8 @@ $horaFin = $_POST['horaFin'];
 $unidades = $_POST['unidades'];
 $correo_profesor = $_SESSION["correo_usuario"];
 
-
-
+$stringIni = '"'.$horaIni.'"';
+$stringFin = '"'.$horaFin.'"';
 
 if ($horaFin < $horaIni) {
     echo "<script type='text/javascript'>alert('La hora de fin del curso no puede ser antes que la hora de inicio.');location='p_gestCursos.php';</script>";
@@ -20,8 +20,8 @@ if ($horaFin < $horaIni) {
     $cantidad = pg_num_rows($consulta);
     if ($cantidad === 0) {
         $queryInsert = "INSERT INTO public.cursos(
-	clave, nombre, 'horaIni', 'horaFin', cursos, profesor)
-	VALUES ('$clave', '$nombreCurso',  $horaIni, $horaFin, $unidades, '$correo_profesor');";
+	clave, nombre, $stringIni, $stringFin, cursos, profesor)
+	VALUES ('$clave', '$nombreCurso',  '$horaIni', '$horaFin', $unidades, '$correo_profesor');";
         pg_query($queryInsert);
          echo "<script type='text/javascript'>alert('Curso registrado exitosamente');location='p_gestCursos.php';</script>";
     }else {
