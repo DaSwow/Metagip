ad48301dca010fdac6214f70b39820ecc06ccd4e
MichaelEAL <amrasdrako@gmail.com>
2021-11-11 01:05:33 -0700

    test

--- p_menu.php
+++ p_menu.php
@@ -18,7 +18,7 @@ session_start();
             <div class="menu-container">
                 <div class="menu">
                     <h2>Menu Principal</h2>
-                    <h1><?php echo 'bienvenido ' .$_SESSION["nombre_usuario"]; ?></h1>
+                    <h3><?php echo 'bienvenido ' .$_SESSION["nombre_usuario"]; ?></h3>
                     <form action="">
                         <input type="submit" class="submnit" formaction="p_gestAlumnos.php" value="Gestionar Alumnos">
                         <input type="submit" class="submnit" formaction="p_gestCursos.php" value="Gestionar Cursos">
