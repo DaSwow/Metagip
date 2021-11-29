1534f52d6b1ae3450a08055028cc106f03e75b30
Marxdd <63327100+Marxdd@users.noreply.github.com>
2021-11-29 13:03:58 -0700

    Update s_login.php

--- s_login.php
+++ s_login.php
@@ -12,14 +12,14 @@ $query = "SELECT * FROM public.usuarios WHERE correo='$mail' AND contrasenia='$p
 $consulta = pg_query($conexion, $query);
 $cantidad = pg_num_rows($consulta);
 $nombre = pg_fetch_row($consulta);
-echo $consulta;
+echo $cantidad;
 
-//if ($cantidad === 1) {
-  //  $_SESSION["nombre_usuario"]=$nombre[2];
-  //  $_SESSION["correo_usuario"]=$nombre[0];
-    //echo "<script type='text/javascript'>location='p_menu.php';</script>";
-//} else {
-    //echo "<script type='text/javascript'>alert('Datos incorrectos');location='index.php';</script>";
-//}
+if ($cantidad === 1) {
+    $_SESSION["nombre_usuario"]=$nombre[2];
+    $_SESSION["correo_usuario"]=$nombre[0];
+    echo "<script type='text/javascript'>location='p_menu.php';</script>";
+} else {
+    echo "<script type='text/javascript'>alert('Datos incorrectos');location='index.php';</script>";
+}
 
 ?>
