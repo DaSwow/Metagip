0386cfafd1be66222197275d4210b1b3d0cd8cc1
DaSwow <carls09995@hotmail.com>
2021-11-29 08:49:12 -0700

    Eliminar

--- s_EliminarUnidades.php
+++ s_EliminarUnidades.php
@@ -9,9 +9,6 @@ $stringClave = '"' . "claveCurso" . '"';
 $query = "DELETE FROM public.unidades WHERE $stringClave = '$clave'";
 $consulta = pg_query($conexion, $query);
 
-if (!$consulta) {
-    echo "<script type='text/javascript'>alert('Datos incorrectos');location='p_gestCursos.php';</script>";
-} else {
-    echo "<script type='text/javascript'>alert('Curso eliminado');location='p_gestCursos.php';</script>";
-}
+    echo "<script type='text/javascript'>location='p_gestCursos.php';</script>";
+
 ?>
