42e18502a93720edc202efe77ced268a5d077509
Marxdd <63327100+Marxdd@users.noreply.github.com>
2021-11-29 13:21:51 -0700

    Update s_regAlumnos.php

--- s_regAlumnos.php
+++ s_regAlumnos.php
@@ -4,18 +4,18 @@ session_start();
 $clave = $_POST['curso'];
 $idAlumno = $_POST['alumno'];
 
-//hola
-$query = 'SELECT * FROM public.rel_cursos_alumnos WHERE "claveCurso"='.$clave.' AND "idAlumno"='.$idAlumno.';';
+//
+$query = "SELECT * FROM public.rel_cursos_alumnos WHERE claveCurso='$clave' AND idAlumno='$idAlumno';";
 $consulta = pg_query($conexion, $query);
 //$cantidad = pg_num_rows($consulta);
-echo $query;
-    // if ($cantidad === 0) {
-       // $queryInsert = "INSERT INTO public.rel_cursos_alumnos(claveCurso, idAlumno) VALUES ('$clave', '$idAlumno);";
+    echo $query;
+  //   if ($cantidad === 0) {
+    //    $queryInsert = "INSERT INTO public.rel_cursos_alumnos(claveCurso, idAlumno) VALUES ('$clave', '$idAlumno);";
       //  pg_query($queryInsert);
-     //   echo "<script type='text/javascript'>alert('Alumn@ registrado al curso exitosamente');location='../p_regAlumnos.php';</script>";
+       // echo "<script type='text/javascript'>alert('Alumn@ registrado al curso exitosamente');location='../p_regAlumnos.php';</script>";
    // } else {
-   //     echo "<script type='text/javascript'>alert('Este alumn@ ya esta registrado');location='../p_regAlumnos.php';</script>";
-  //  }
+     //   echo "<script type='text/javascript'>alert('Este alumn@ ya esta registrado');location='../p_regAlumnos.php';</script>";
+   // }
 ?>
 
 
