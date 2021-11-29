53f9c82e51772ad0dbdbd9d08d2c043103e17b91
Marxdd <63327100+Marxdd@users.noreply.github.com>
2021-11-29 13:30:11 -0700

    Update s_regAlumnos.php

--- s_regAlumnos.php
+++ s_regAlumnos.php
@@ -10,13 +10,13 @@ $query = "SELECT * FROM public.rel_cursos_alumnos WHERE ($stringClaveCurso='$cla
 $consulta = pg_query($conexion, $query);
 $cantidad = pg_num_rows($consulta);
     echo $cantidad;
-  //   if ($cantidad === 0) {
-    //    $queryInsert = "INSERT INTO public.rel_cursos_alumnos(claveCurso, idAlumno) VALUES ('$clave', '$idAlumno);";
-      //  pg_query($queryInsert);
-       // echo "<script type='text/javascript'>alert('Alumn@ registrado al curso exitosamente');location='../p_regAlumnos.php';</script>";
-   // } else {
-     //   echo "<script type='text/javascript'>alert('Este alumn@ ya esta registrado');location='../p_regAlumnos.php';</script>";
-   // }
+     if ($cantidad === 0) {
+        $queryInsert = "INSERT INTO public.rel_cursos_alumnos(claveCurso, idAlumno) VALUES ('$clave', '$idAlumno);";
+        pg_query($queryInsert);
+        echo "<script type='text/javascript'>alert('Alumn@ registrado al curso exitosamente');location='../p_regAlumnos.php';</script>";
+    } else {
+        echo "<script type='text/javascript'>alert('Este alumn@ ya esta registrado');location='../p_regAlumnos.php';</script>";
+    }
 ?>
 
 
