51c0935a12ad8083cafb84b843fc7decf44a2bb1
MichaelEAL <amrasdrako@gmail.com>
2021-11-29 06:15:54 -0700

    test2

--- p_regAlumnos.php
+++ p_regAlumnos.php
@@ -16,37 +16,39 @@ $consulta = pg_query($conexion, $query);
                 <h2>Registrar Alumnos</h2>
                 <div class="cursos">
                     <p>Curso:</p>
-                    <input type="text" placeholder="Curso" class="curso">
-                    <select name="nombres" id="nombresAlumnos">
+                    <p><input type="text" placeholder="Curso" class="curso">
+                    <select name="nombres">
                         <option value="materia">Metodologia Agiles</option>
                 </div>
                 <div class="buscador">
                     <input type="text" placeholder="ID" class="idC">
-                    <select name="nombre" id="nombresA">
+                    <select name="nombre">
                         <option value="a1">Jesus Edith Carballo Herrera</option>
                         <option value="a2">Hiram Rodriguez</option>
-                        <input type="submit" class="submit" value="Agregar"> 
-                        </div>
+					</select>
+					<form action="p_regAlumnos.php" method="post">
+							<input type="submit" class="submit" value="Agregar"> 
+					</form>
+                </div>
                         <div id="scroll2">
-
                             <table class="table2"border="1">
                                 <tr>
-                                    <td class="filaID"><p>ID</p></td>
-                                    <td class="filaAl"><p>Alumno</p></td>
-                                    <td class="filaF"><p> </p></td>
+                                    <td class="fila1"><p>ID</p></td>
+                                    <td class="fila"><p>Alumno</p></td>
                                 </tr>
                                 <tr>
-                                    <td class="fila2ID"><p>00000189362</p></td>
-                                    <td class="fila2A"><p>Jesus Edith Carballo Herrera</p></td>
+                                    <td class="fila2"><p>00000189362</p></td>
+                                    <td class="fila2"><p>Jesus Edith Carballo Herrera</p></td>
                                     <td class="fila2">
                                         <input type="checkbox" id="cbox" value="first">
                                     </td>
                                 </tr>
                             </table>
                             <nav id="btn">
-                                <form action="">
-
+                                <form action="p_regAlumnos.php" method="post">
                                     <input type="submit" class="submit" value="Eliminar">
+                                </form>
+                                <form action="p_regAlumnos.php" method="post">
                                     <input type="submit" class="submit" value="Guardar">
                                 </form>
                                 <form action="p_menu.php" method="post">
