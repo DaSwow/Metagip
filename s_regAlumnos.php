7fe46c99492d4b134a37a9a35481d2f2d7635a11
Marxdd <63327100+Marxdd@users.noreply.github.com>
2021-11-29 12:04:13 -0700

    Update s_regAlumnos.php

--- s_regAlumnos.php
+++ s_regAlumnos.php
@@ -5,10 +5,10 @@ $clave = $_POST['curso'];
 $idAlumno = $_POST['alumno'];
 
 
-$query = "SELECT * FROM public.rel_cursos_alumnos WHERE claveCurso='$clave'AND idAlumno='$idAlumno';";
-$consulta = pg_query($conexion, $query);
-$cantidad = pg_num_rows($consulta);
-    echo $cantidad;
+//$query = "SELECT * FROM public.rel_cursos_alumnos WHERE claveCurso='$clave'AND idAlumno='$idAlumno';";
+//$consulta = pg_query($conexion, $query);
+//$cantidad = pg_num_rows($consulta);
+    echo $idAlumno;
     // if ($cantidad === 0) {
       //  $queryInsert = "INSERT INTO public.rel_cursos_alumnos(claveCurso, idAlumno) VALUES ('$clave', '$idAlumno);";
         //pg_query($queryInsert);
