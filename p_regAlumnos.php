42cdce6d52849d3191987143199d09309fc75202
DaSwow <carls09995@hotmail.com>
2021-11-30 17:23:05 -0700

    prueba desplegar lista

--- p_regAlumnos.php
+++ p_regAlumnos.php
@@ -3,73 +3,73 @@ include 'db.php';
 session_start();
 $correo_prof = $_SESSION['correo_usuario'];
 
+
 $query = "SELECT * FROM public.alumnos WHERE profesor='$correo_prof'";
 $queryCurso = "SELECT * FROM public.cursos WHERE profesor='$correo_prof'";
 $consultaCurso = pg_query($conexion, $queryCurso);
-$consulta = pg_query($conexion, $query);
+$consultaAlumno = pg_query($conexion, $query);
+
+$alumnosEnCurso = $_GET['alumnos'];
 ?>
 <!DOCTYPE html>
-<link rel="stylesheet" href="style.css">
-<title>Registrar Alumnos</title>
-<body>
-    <div class="container">
-        <div class="table-container3">
-            <div class="menu">
-                <h2>Registrar Alumnos</h2>
-                <form action="s_regAlumnos.php" method="post">
-                    <div class="cursos">
-                        <p>Curso:</p>
-                        <p><input type="text" placeholder="Curso" class="curso">
-                        <select name="curso">
-                             <?php  while ($rowCurso = pg_fetch_assoc($consultaCurso)) { ?>
-                            <option value="<?php echo $rowCurso['clave']; ?>"><?php echo $rowCurso['nombre']; ?></option>
-                              <?php } ?>
-                        </select>
-                    </div>
-                    <div class="buscador">
-                        <input type="text" placeholder="ID" class="idC">
-                        <select name="alumno">
-                            <?php  while ($row = pg_fetch_assoc($consulta)) { ?>
-                            <option value="<?php echo $row['id']; ?>"><?php echo $row['nombre']; ?></option>
+<html>
+    <link rel="stylesheet" href="style.css">
+    <title>Registrar Alumnos</title>
+    <body>
+        <div class="container">
+            <div class="table-container3">
+                <div class="menu">
+                    <h2>Registrar Alumnos</h2>
+                    <form action="s_regAlumnos.php" method="post">
+                        <div class="cursos">
+                            <p>Curso:</p>
+                            <p><input type="text" placeholder="Curso" class="curso">
+                                <select name="curso" required>
+                                    <?php while ($rowCurso = pg_fetch_assoc($consultaCurso)) { ?>
+                                        <option value="<?php echo $rowCurso['clave']; ?>"><?php echo $rowCurso['nombre']; ?></option>
+                                    <?php } ?>
+                                </select>
+                                <br><br>
+                        </div>
+                        <div class="buscador">
+                            <input type="text" placeholder="ID" class="idC">
+                            <select name="alumno">
+                                <?php while ($row = pg_fetch_assoc($consultaAlumno)) { ?>
+                                    <option value="<?php echo $row['id']; ?>"><?php echo $row['nombre']; ?></option>
+                                <?php } ?>
+                            </select>
+                            <br><br>
+                        </div>
+                        <input type="submit" class="submit"  action="btn_agregar"   value="Agregar"> 
+                        <input type="submit" class="submit"  action="btn_desplegar" value="Desplegar Lista">
+                    </form>
+                    <div id="scroll2">
+                        <table class="table2"border="1">
+                            <?php while ($row = pg_fetch_assoc($alumnosEnCurso)) { ?>
+                                <tr>
+                                    <td class="fila1"><p>ID</p></td>
+                                    <td class="fila"><p>Alumno</p></td>
+                                </tr>
+                                <tr>
+                                    <td class="fila2"><p><?php echo $row['id']; ?></p></td>
+                                    <td class="fila2"><p><?php echo $row['nombre']; ?></p></td>
+                                </tr>
                             <?php } ?>
-                        <!--<option value="a1">Jesus Edith Carballo Herrera</option>
-                        <option value="a2">Hiram Rodriguez</option>-->
-                        </select>
-                    
+                        </table>
+                        <nav id="btn">
+                            <form action="p_regAlumnos.php" method="post">
+                                <input type="submit" class="submit" value="Eliminar">
+                            </form>
+                            <form action="p_menu.php" method="post">
+                                <input type="submit" class="submit" value="Regresar">
+                            </form>
+                        </nav> 
+
                     </div>
-                        <input type="submit" class="submit" value="Agregar"> 
-                </form>
-                 <form action="p_regAlumnos.php?cursoSeleccionado=<?php echo $rowCurso['clave'] ?>" method="post">
-                            <input type="submit" class="submit" value="Desplegar Lista">
-                </form>
-                <div id="scroll2">
-                    <table class="table2"border="1">
-                        <tr>
-                            <td class="fila1"><p>ID</p></td>
-                            <td class="fila"><p>Alumno</p></td>
-                        </tr>
-                        <tr>
-                            <td class="fila2"><p>00000189362</p></td>
-                            <td class="fila2"><p>Jesus Edith Carballo Herrera</p></td>
-                            <td class="fila2">
-                                <input type="checkbox" id="cbox" value="first">
-                            </td>
-                        </tr>
-                    </table>
-                    <nav id="btn">
-                        <form action="p_regAlumnos.php" method="post">
-                            <input type="submit" class="submit" value="Eliminar">
-                        </form>
-                        <form action="p_menu.php" method="post">
-                            <input type="submit" class="submit" value="Regresar">
-                        </form>
-                    </nav> 
 
                 </div>
 
             </div>
-
         </div>
-    </div>
-</body>
+    </body>
 </html>
--- s_regAlumnos.php
+++ s_regAlumnos.php
@@ -1,22 +1,32 @@
 <?php
+
 require 'db.php';
 session_start();
 $clave = $_POST['curso'];
 $idAlumno = $_POST['alumno'];
 $stringClaveCurso = '"' . "claveCurso" . '"';
 $stringIdAlumno = '"' . "idAlumno" . '"';
-//
-$query = "SELECT * FROM public.rel_cursos_alumnos WHERE ($stringClaveCurso='$clave') AND ($stringIdAlumno='$idAlumno');";
-$consulta = pg_query($conexion, $query);
-$cantidad = pg_num_rows($consulta);
+if (isset($_POST['btn_agregar'])) {
+    //
+    $query = "SELECT * FROM public.rel_cursos_alumnos WHERE ($stringClaveCurso='$clave') AND ($stringIdAlumno='$idAlumno');";
+    $consulta = pg_query($conexion, $query);
+    $cantidad = pg_num_rows($consulta);
+
     echo $cantidad;
-     if ($cantidad === 0) {
+    if ($cantidad === 0) {
+
         $queryInsert = "INSERT INTO public.rel_cursos_alumnos($stringClaveCurso, $stringIdAlumno) VALUES ('$clave','$idAlumno');";
         pg_query($queryInsert);
         echo "<script type='text/javascript'>alert('Alumn@ registrado al curso exitosamente');location='../p_regAlumnos.php';</script>";
     } else {
         echo "<script type='text/javascript'>alert('Este alumn@ ya esta registrado');location='../p_regAlumnos.php';</script>";
     }
+} else if (isset($_POST['btn_desplegar'])) {
+    $queryAlumnosDelCurso = "SELECT * FROM public.rel_cursos_alumnos WHERE ($stringClaveCurso='$clave');";
+    $stringId= '"' . "id" . '"';
+    $$stringId= '"' . "nombre" . '"';
+    $queryNombreAlumno = "SELECT $stringId,$stringId FROM public.alumnos INNER JOIN (SELECT * FROM public.rel_cursos_alumnos where $stringClaveCurso='ABC512') AS curso ON (alumnos.id = $stringIdAlumno);";
+    $consulta = pg_query($conexion, $queryAlumnosDelCurso);
+    echo "<script type='text/javascript'>location='../p_regAlumnos.php?alumnos=$consulta';</script>";
+}
 ?>
-
-
