9b8b724bd8db7b321842279d5ede266cdf0b1e38
DaSwow <carls09995@hotmail.com>
2021-11-30 17:43:33 -0700

    Update s_regAlumnos.php

--- s_regAlumnos.php
+++ s_regAlumnos.php
@@ -24,8 +24,8 @@ if ($accion==="agregar") {
     }
 } else if ($accion==="desplegar") {
     $stringId= '"' . "id" . '"';
-    $stringId= '"' . "nombre" . '"';
-    $queryAlumnos = "SELECT $stringId,$stringId FROM public.alumnos INNER JOIN (SELECT * FROM public.rel_cursos_alumnos where $stringClaveCurso=$clave) AS curso ON (alumnos.id = $stringIdAlumno);";
+    $stringNombre= '"' . "nombre" . '"';
+    $queryAlumnos = "SELECT $stringId,$stringNombre FROM public.alumnos INNER JOIN (SELECT * FROM public.rel_cursos_alumnos where $stringClaveCurso=$clave) AS curso ON (alumnos.id = $stringIdAlumno);";
     $consultaAlumnos = pg_query($conexion, $queryAlumnos);
     echo "<script type='text/javascript'>location='../p_regAlumnos.php?alumnos=$consultaAlumnos;</script>";
 }
